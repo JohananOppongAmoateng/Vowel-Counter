d8735d6b11323d1e3d2a13f6374b96de78cb8cb6
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2021-12-25 23:53:15 +0000

    done left with css

---
+++ script.js
@@ -0,0 +1,19 @@
+const input = document.getElementById("inputText")
+const check = document.getElementById("check")
+
+function vowelCounter(string) {
+    let num = 0
+    for (let i= 0;i<string.length;i++){
+            let str = string.charAt(i)
+            let news = str.toLowerCase()
+            if ( news=== "a"|| news === "e" || news === "i" || news === "o" || news === "u"){
+                num += 1
+        }
+    }
+    alert(num)
+}
+
+// let vowel = vowelCounter(input.value)
+check.addEventListener("click",function () {
+    vowelCounter(input.value)
+})
